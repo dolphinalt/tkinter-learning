cf159a699d2838318607d5e332008c2ecdd14492
dolphinalt <ethan.zhao.us@gmail.com>
2025-06-30 14:54:35 -0700

    proj34

---
+++ 34. stylingModules.py
@@ -0,0 +1,30 @@
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = ctk.CTk()
+window.title('customtkinter app')
+window.geometry('600x400')
+
+# widgets
+string_var = ctk.StringVar(value = 'a custom string')
+label = ctk.CTkLabel(window, text = 'A ctk label', fg_color = ('blue', 'red'), text_color = ('black', 'white'), corner_radius = 10, textvariable = string_var)
+label.pack()
+
+button = ctk.CTkButton(window, text = 'A ctk button', fg_color = '#FF0', text_color = '#000', hover_color = '#AA0', command = lambda: ctk.set_appearance_mode('light'))
+button.pack()
+
+frame = ctk.CTkFrame(window)
+frame.pack()
+
+slider = ctk.CTkSlider(frame)
+slider.pack(padx = 20, pady = 20)
+
+# exercise
+# implement the ugly customtkinter switch
+switch = ctk.CTkSwitch(window, text = 'Exercise Switch', corner_radius = 2, switch_width = 60, switch_height = 30, border_color = 'blue', fg_color = ('blue', 'red'), progress_color = 'pink', button_color = 'green', button_hover_color = 'yellow')
+switch.pack()
+
+# run
+window.mainloop()
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Tkinter event bindings
 https://www.pythontutorial.net/tkinter/tkinter-event-binding/
 
+## CustomTkinter docs
+https://customtkinter.tomschimansky.com/documentation/
+
 ## Progress
 
 https://www.youtube.com/watch?v=mop6g-c5HEY
